dd5ca8afeb58147813ec4a4145ed28795568aae5
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-11 09:57:15 +0530

    [RK Nanda] Checking Leap Year and adding menu

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace BasicCorePrograms
+{
+	public class LeapYear
+	{
+		public bool CheckLeapYear(int year)
+		{
+			if(year%400 == 0)
+			{
+				return true;
+			}
+			else if(year%100 == 0)
+			{
+				return false;
+
+			}
+
+			else if(year % 4 == 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -5,23 +5,51 @@ class Program
     {
         int option;
         Console.WriteLine("Basic Core Programs");
-        Console.WriteLine($"Enter option:");
-        option = Convert.ToInt32(Console.ReadLine());
-
-
-        switch(option)
+        bool flag = true;
+        while(flag)
         {
-            case 1:
-                {
-                    FlipCoin f = new FlipCoin();
-                    f.FindPercentage();
-                    break;
-                }
+            Console.WriteLine("1. FlipCoin");
+            Console.WriteLine("2. LeapYear");
+            Console.WriteLine("0. Exit");
+            option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 0:
+                    {
+                        flag = false;
+                        break;
+                    }
+                case 1:
+                    {
+                        FlipCoin f = new FlipCoin();
+                        f.FindPercentage();
+                        break;
+                    }
 
+                case 2:
+                    {
+                        int year;
+                        bool res;
+                        Console.WriteLine("Checking If Leap Year");
+                        Console.WriteLine("Enter year to check");
+                        year = Convert.ToInt32(Console.ReadLine());
+                        LeapYear l1 = new LeapYear();
+                        res = l1.CheckLeapYear(year);
+                        if (res)
+                        {
+                            Console.WriteLine($"{year} is a Leap Year");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{year} is Not a Leap Year");
+                        }
+                        break;
+
+                    }
+
+            }
 
         }
-       
-
 
     }
 }
