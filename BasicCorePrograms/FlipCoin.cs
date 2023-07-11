1ab308012d42be7cba42dcd735b72bb13120755f
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-11 09:37:22 +0530

    [RK Nanda] Adding FlipCoin class

---
+++ BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace BasicCorePrograms
+{
+	public class FlipCoin
+	{
+		public void FindPercentage()
+		{
+			int times;
+			double head = 0;
+            double tail = 0;
+			double headPercentage = 0;
+			Random rn = new Random();
+            Console.WriteLine("Implementing Flip Coin");
+			Console.WriteLine("Enter Number of times you want to flip the coin");
+            times = Convert.ToInt32(Console.ReadLine());
+
+			for(int i = 0; i<times;i++)
+			{
+				double number = rn.NextDouble();
+				if(number < 0.5)
+				{
+					tail++;
+				}
+				else
+				{
+					head++;
+				}
+			}
+
+			headPercentage = (head / times) * 100;
+			Console.WriteLine($"% of occurance of Heads is {headPercentage}");
+            Console.WriteLine($"% of occurance of Tails is {100 - headPercentage}");
+
+
+
+        }
+	}
+}
+
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -3,7 +3,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        int option;
         Console.WriteLine("Basic Core Programs");
+        Console.WriteLine($"Enter option:");
+        option = Convert.ToInt32(Console.ReadLine());
+
+
+        switch(option)
+        {
+            case 1:
+                {
+                    FlipCoin f = new FlipCoin();
+                    f.FindPercentage();
+                    break;
+                }
+
+
+        }
+       
 
 
     }
